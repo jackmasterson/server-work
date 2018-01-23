ab3b56bbbb2a6569d54cbf09d8459e4c730542c8
JackMasterson <jackmasterson5@gmail.com>
2018-01-22 20:04:42 -0500

    first commit - server running

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+
+console.log('app is listening on port: ', PORT);
+
+app.get('*', (req, res) => {
+    res.send('hello world!');
+})
